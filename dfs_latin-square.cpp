00e1780304139cdb30c2fa32f4263de08da62809
Veronë Krasniqi <75127287+VeRoNa-KrAsNiQi@users.noreply.github.com>
2022-06-06 23:35:27 +0200

    added methods addEdge and DFS

--- dfs_latin-square.cpp
+++ dfs_latin-square.cpp
@@ -11,3 +11,17 @@ public:
     void createGraph(int n, Graph &g);
     void DFS(int v);
 };
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w);
+}
+
+void Graph::DFS(int v)
+{
+    visited[v] = true;
+    cout << v << " ";
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFS(*i);
+}
